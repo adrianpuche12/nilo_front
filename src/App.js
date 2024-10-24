22116340c6545e929a6e26b9b062d52555bd96c5
Pialoenor12# <jorgeadrian_pucheta@hotmail.com>
2024-10-24 12:57:29 -0300

    agrego componente register, lista de usuarios y contries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Countries from './Components/Countries';
 import Register from './Components/Registrer';
+import Users from './Components/Users';
+
 function App() {
   return (
     <>
       <Register />
+      <Countries />
+      <Users />
+
     </>
   );
 }
