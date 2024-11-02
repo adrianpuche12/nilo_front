ec5bf185236c485983fc4f9c676ee37e03370f9b
leonel2789 <leofer-acosta.755@hotmail.com>
2024-11-02 16:15:06 -0300

    Modularizacion de rutas

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AllUsers from "./Components/User/AllUsers";
-import UserDetail from './Components/User/UserDetail';
-import Register from './Components/Register';
-import Home from "./Components/Home";
-import CountryDetail from "./Components/Country/CountryDetail ";
-import Countries from "./Components/Country/Countries";
-import CreateCountry from "./Components/Country/CreateCountry";
-import Activities from "./Components/Activities/Activities";
-import Province from "./Components/Province";
-import Itineraries from "./Components/Itineraries/Itineraries";
-
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Outlet } from "react-router-dom";
+import AppRoutes from './Utils/AppRoutes';
 
 function App() {
   return (
     <>
-      < Outlet />
-
+      <Outlet />
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/user/:id" element={<UserDetail />} />
-          <Route path="/countries" element={<Countries />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/users" element={<AllUsers />} />
-          <Route path="/country-detail/:id" element={<CountryDetail />} />
-          <Route path="/create-country" element={<CreateCountry />} />
-          <Route path="/activities" element={<Activities />} />
-          <Route path="/province" element={<Province />} />
-          <Route path="/itineraries" element={<Itineraries />} />
-        </Routes>
+        <AppRoutes />
       </Router>
-
     </>
   );
 }
--- src/Utils/AppRoutes.jsx
+++ src/Utils/AppRoutes.jsx
@@ -1,3 +1,31 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import AllUsers from '../Components/User/AllUsers';
+import UserDetail from '../Components/User/UserDetail';
+import Register from '../Components/Register';
+import Home from '../Components/Home';
+import CountryDetail from '../Components/Country/CountryDetail ';
+import Countries from '../Components/Country/Countries';
+import CreateCountry from '../Components/Country/CreateCountry';
+import Activities from '../Components/Activities/Activities';
+import Province from '../Components/Province';
+import Itineraries from '../Components/Itineraries/Itineraries';
 
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/user/:id" element={<UserDetail />} />
+      <Route path="/countries" element={<Countries />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/users" element={<AllUsers />} />
+      <Route path="/country-detail/:id" element={<CountryDetail />} />
+      <Route path="/create-country" element={<CreateCountry />} />
+      <Route path="/activities" element={<Activities />} />
+      <Route path="/province" element={<Province />} />
+      <Route path="/itineraries" element={<Itineraries />} />
+    </Routes>
+  );
+};
 
 export default AppRoutes;
